6a792e863023cd3eca66042bce9b603393ebeb9d
Wholeme3 <168391691+Wholeme3@users.noreply.github.com>
2024-05-05 23:38:45 -0500

    Add files via upload

---
+++ #Passport Bros Mattress Assassins App.py
@@ -0,0 +1,16 @@
+#Passport Bros Mattress Assassins App
+
+print('Thank You for downloading The Passport Matress Assassins App!')
+print('Please follow directions and answer all questions to gain access to this amazing travel tool!')
+
+name=input('Please Enter Your Name To Begin')
+
+while name=="":
+
+    name=input('Enter Your Name Please:')
+    print(f'Welcome {name} Your Journey Around the \World Starts Now!') 
+
+name=Int(input('You must be atleast 18 to use the app', 'Enter your age'))
+while age>=18:
+    print(f'Thanks for confirming you are {age} years old and of age to use the app!')
+    
